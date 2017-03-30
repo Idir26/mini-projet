f66278c538df5fd592c82a14d1e9f1360c932c75
groupe-vanilla <vanilla@chalearn.org>
2017-03-30 19:43:30 +0200

    Update DataManagerTest.py

--- my_code/DataManagerTest.py
+++ my_code/DataManagerTest.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 11 08:04:23 2017
 
-@author: isabelleguyon
-
-This is an example of program that tests the Iris challenge Data Manager class.
-Another style is to incorporate the test as a main function in the Data manager class itself.
-"""
 from zDataManager import zDataManager
 input_dir = "../public_data"
 output_dir = "../res"
 
     
-basename = 'Iris'
+basename = 'movierec'
 D = zDataManager(basename, input_dir)
 print D
     
